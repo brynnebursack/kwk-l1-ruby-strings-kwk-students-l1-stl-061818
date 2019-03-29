f4c0e4a340a58b1986098dc36bc74550f0cb8784
brynnebursack <brynnebursack@gmail.com>
2019-03-29 14:23:35 +0000

    Automatically backed up by Learn

--- invitation.rb
+++ invitation.rb
@@ -1,11 +1,12 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-# guest_name = "Hermione"
-# party_name = "Best Flag Day Party Ever"
-# date = "June 14"
-# time = "6pm"
-# host_name = "Harry Potter"
+
+guest_name = "Hermione"
+party_name = "Best Flag Day Party Ever"
+date = "June 14"
+time = "6pm"
+host_name = "Harry Potter"
 
 def invitation
 puts "Dear #{guest_name},
@@ -15,3 +16,5 @@ You are invited to the #{party_name} on #{date} at #{time}. Please RSVP no later
 Sincerely,
 
 #{host_name}"
+
+end
